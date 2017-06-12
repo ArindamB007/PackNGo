d6155b217742e5532a738cccb3350b4e5430ca60
ArindamB007 <arindam.bandyopadhyay@gmail.com>
2017-06-12 19:43:26 +0530

    Signup service - WIP2

--- src/main/java/com/png/data/entity/User.java
+++ src/main/java/com/png/data/entity/User.java
@@ -4,6 +4,8 @@ import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 
+import java.util.Set;
+
 import javax.persistence.*;
 
 /**
@@ -12,11 +14,11 @@ import javax.persistence.*;
 
 @Entity
 @Table(name="user")
-public class user {
+public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id")
-    private Long id;
+    @Column(name = "id_user")
+    private Long id_user;
 
     @Column (name="email")
     @Email(message = "Please provide a valid email")
@@ -31,5 +33,69 @@ public class user {
     @Transient
     private String password;
 
+    @Column(name="first_name")
+    private String firstName;
 
+    @Column(name="last_name")
+    private String lastName;
+
+    @Transient
+    private String confirmPassword;
+
+
+	@ManyToMany
+    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "id_user"), inverseJoinColumns = @JoinColumn(name = "id_role"))
+    private Set<Role> roles;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+    public Long getId_user() {
+		return id_user;
+	}
+
+	public void setId_user(Long id_user) {
+		this.id_user = id_user;
+	}
+
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
+
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
 }
