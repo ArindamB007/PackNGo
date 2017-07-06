50679fdd017da4e01b689a913e858f4801e1bfa7
ArindamB007 <arindam.bandyopadhyay@staples.com>
2017-07-06 08:46:24 +0530

    Session Management - V1

---
+++ src/main/java/com/png/config/ApplicationConfiguration_old1.java
@@ -0,0 +1,83 @@
+package com.png.config;
+
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+/*@Configuration
+public class ApplicationConfiguration extends WebMvcConfigurerAdapter implements ApplicationContextAware {
+
+  private ApplicationContext applicationContext;
+
+  public void setApplicationContext(ApplicationContext applicationContext) {
+    this.applicationContext = applicationContext;
+  }
+  
+  @Bean
+  ServletRegistrationBean h2servletRegistration(){
+      ServletRegistrationBean registrationBean = new ServletRegistrationBean( new WebServlet());
+      registrationBean.addUrlMappings("/h2-console/*");
+      return registrationBean;
+  }
+ 
+
+  @Bean
+  public ViewResolver internalResourceViewResolver() {
+      InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+      //resolver.setPrefix("/resources/static/");
+      //resolver.setApplicationContext(applicationContext);
+      //resolver.setViewNames("/views/*");
+      resolver.setViewNames(new String[] {"/views/*"});
+      resolver.setOrder(0);
+      resolver.setSuffix(".html");
+      return resolver;
+  }
+
+  @Override
+  public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+	  configurer.enable();
+  }
+  
+  
+  @Bean
+  public ViewResolver viewResolver() {
+    ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+    resolver.setTemplateEngine(templateEngine());
+    resolver.setCharacterEncoding("UTF-8");
+    resolver.setViewNames(new String[] {"/templates/*"});
+    resolver.setExcludedViewNames(new String [] {"/views/*"});
+    resolver.setOrder(1);
+    return resolver;
+  }
+
+  public TemplateEngine templateEngine() {
+    SpringTemplateEngine engine = new SpringTemplateEngine();
+    engine.setEnableSpringELCompiler(true);
+    engine.setTemplateResolver(templateResolver());
+    return engine;
+  }
+
+  private ITemplateResolver templateResolver() {
+    SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+    resolver.setApplicationContext(applicationContext);
+    resolver.setPrefix("/templates/");
+    resolver.setCheckExistence(true);
+    resolver.setTemplateMode(TemplateMode.HTML);
+    return resolver;
+  } 
+}*/
