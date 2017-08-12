f5f86dffa91c3c59dc9e60d39096ea590e412c18
ArindamB007 <arindam.bandyopadhyay@staples.com>
2017-08-12 20:42:08 +0530

    Session Management - V3 Completed Login handshake

--- src/main/resources/static/services/local-storage-service.js
+++ src/main/resources/static/services/local-storage-service.js
@@ -2,6 +2,9 @@ PackNGo.factory('LocalStorageService',function($localStorage) {
   return{
     setLocalStore : function(localStore, value) {
       $localStorage[localStore] = value;
+    },
+    getLocalStore : function(localStore){
+      return $localStorage[localStore];
     }
   };
 });
