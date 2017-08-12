bb698b26849f09414a5c9f8bd9824a37f97793cc
ArindamB007 <arindam.bandyopadhyay@staples.com>
2017-08-12 20:42:08 +0530

    Session Management - V3 Completed Login handshake

--- src/main/resources/static/controllers/login.js
+++ src/main/resources/static/controllers/login.js
@@ -1,12 +1,13 @@
-PackNGo.controller('LoginCtrl',function($scope,LoginService,LocalStorageService,ModalService){
+PackNGo.controller('LoginCtrl',function($scope,LoginService,LocalStorageService,ModalService,$http){
   $scope.loginDetails = {};
   $scope.doLogin = function() {
     LoginService.userLogin($scope.loginDetails)
       .then(function(response){
       $scope.loginForm.$setPristine();
       $scope.loginDetails = {};
-      LocalStorageService.setLocalStore("Token",response.headers("x-auth-token"));
+      LocalStorageService.setLocalStore("token",response.headers("x-auth-token"));
       console.log(response.headers("x-auth-token"));
+      $http.defaults.headers.common["x-auth-token"] = LocalStorageService.getLocalStore("token");
     })
       .catch(function(response){
       $scope.loginDetails = {};
