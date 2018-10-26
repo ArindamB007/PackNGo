800bb2335321fa3d46f84e60b6f41e85e1b5a483
patraanup123 <patraanup123@gmail.com>
2018-10-26 10:25:52 +0530

    Email confirmation changes and styles AP

--- src/main/resources/static/controllers/verifyemail.js
+++ src/main/resources/static/controllers/verifyemail.js
@@ -1,12 +1,22 @@
 PackNGo.controller('VerifyEmailCtrl',function($scope,$routeParams,LoginService,CommonService){
     $scope.doVerifyEmail = function() {
+        $scope.message = "";
+        $scope.info="";
         LoginService.verifyEmail($routeParams.verificationCode)
             .then(function(response){
                 console.log(response);
+                $scope.message = "";
+                $scope.info="";
+                $scope.info="success_email";
+                $scope.message = " Your Email Validated Successfully";
             })
             .catch(function(response){
                 console.log(response);
-                CommonService.handleDefaultErrorResponse("md","Error Validating Email", response,["OK"]);
+                //.handleDefaultErrorResponse("md","Error Validating Email", response,["OK"]);
+                $scope.message = "";
+                $scope.info="";
+                $scope.info="error_email";
+                $scope.message = "We are facing some error validating your email id";
             });
     };
     $scope.doVerifyEmail();
